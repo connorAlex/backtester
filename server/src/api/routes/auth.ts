b42b6149b0453f92981a3177614180f418404dd7
connorAlex <alex.connor.cochrane@gmail.com>
2023-04-06 11:54:01 -0500

    add secret .env

--- server/src/api/routes/auth.ts
+++ server/src/api/routes/auth.ts
@@ -1,8 +1,9 @@
+import config from '../../config/index';
 
 export const authConfig =  {
     authRequired: false,
     auth0Logout: true,
-    secret: 'long rando generated string stored in env',
+    secret: config.auth_secret,
     baseURL: 'http://localhost:8080',
     clientID: 'nZ2qeCDfyGjj2xHtnqni3mkv3MdH7bPf',
     issuerBaseURL: 'https://dev-qrgij8v8365i5ipr.us.auth0.com'
--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -9,5 +9,6 @@ if (env.error) {
 export default {
     db_url: process.env.DB_URL,
     db_users: process.env.DB_COLL,
-    db_name: process.env.DB_NAME
+    db_name: process.env.DB_NAME,
+    auth_secret: process.env.AUTH_SECRET
 }
