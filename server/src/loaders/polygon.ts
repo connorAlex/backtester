b7e3786993e4634255778672e4d87d30db3ce8f5
connorAlex <alex.connor.cochrane@gmail.com>
2023-04-12 13:17:55 -0500

    refactor ifs

--- server/src/loaders/polygon.ts
+++ server/src/loaders/polygon.ts
@@ -3,17 +3,9 @@ const aggregate = "https://api.polygon.io/v2/aggs"
 
 const getAggregate = (obj: userAggregateRequest): String => {
     let url = aggregate +`/ticker/${obj.stocksTicker}/range/${obj.multiplier}/${obj.timespan}/${obj.from}/${obj.to}?`;
-
-    if (obj.adjusted) {
-        url += `adjusted=${obj.adjusted}`;
-    }
-    if (obj.sort){
-        url += `&sort=${obj.sort}`;
-    }
-    if (obj.limit) { 
-        url += `&limit=${obj.limit}`;
-    }
-
+    url += obj.adjusted? `adjusted=${obj.adjusted}` : '';
+    url += obj.sort? `&sort=${obj.sort}`: '';
+    url += obj.limit? `&limit=${obj.limit}` : '';
     url += `&apiKey=${config.poly_key}`;
 
     return url;
