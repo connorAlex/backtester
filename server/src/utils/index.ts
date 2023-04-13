c33f1096f93706c0f2297cd906066bb276bc1a4a
connorAlex <alex.connor.cochrane@gmail.com>
2023-04-13 10:43:05 -0500

    add fetch response funct

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -1,6 +1,12 @@
-const request = async (url:URL): Promise<Object> => {
-    const res = await fetch(url);
-    const data = await res.json();
+const request = async (url:URL): Promise<aggregateResponse> => {
+    const res = await fetch(url, {
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+    });
+    
+    const data: aggregateResponse = await res.json();
 
     return data;
 }
