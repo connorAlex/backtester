0227bdfbc639e1c4b943b87bad1b7694fa4aed20
connorAlex <alex.connor.cochrane@gmail.com>
2023-04-13 15:34:15 -0500

    update dataframe testing

--- server/src/tests/jsonLoader.test.ts
+++ server/src/tests/jsonLoader.test.ts
@@ -1,11 +1,8 @@
 import { describe, assert, expect, it, beforeAll, afterAll} from 'vitest';
 import { getAggregate } from '../loaders/polygon';
 import { request } from '../utils';
-import * as dataForge from "data-forge";
-import "data-forge-fs";
-import "data-forge-indicators"
+import { formatResponse } from '../backtst/utils';
 
-//let jsonSeries = dataForge.readFileSync("sample_agg.json").parseJSON();
 const aggregateRequest: userAggregateRequest = {
     stocksTicker: "AAPL",
     multiplier: 1,
@@ -20,9 +17,8 @@ const aggregateRequest: userAggregateRequest = {
 describe("Load in market JSON Data", () => {
     it("Parse JSON to DataFrame", async () => {
         const res = await request(getAggregate(aggregateRequest));
-        //console.log(res);
-        // const data = new dataForge.DataFrame(await res);
-        // console.log(await data);
+        const data = await formatResponse(res);
+        expect(await data.toArray()[0]["open"]).toEqual(130.465);
     })
 })
     
