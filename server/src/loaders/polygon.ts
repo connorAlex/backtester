c884b3a667e674aea133385e300258a79dff9f85
connorAlex <alex.connor.cochrane@gmail.com>
2023-04-13 15:34:51 -0500

    var name change

--- server/src/loaders/polygon.ts
+++ server/src/loaders/polygon.ts
@@ -1,15 +1,17 @@
 import config from '../config/index';
-const aggregate = "https://api.polygon.io/v2/aggs"
+const base_url = "https://api.polygon.io/v2"
 
-const getAggregate = (obj: userAggregateRequest): String => {
-    let url = aggregate +`/ticker/${obj.stocksTicker}/range/${obj.multiplier}/${obj.timespan}/${obj.from}/${obj.to}?`;
+const getAggregate = (obj: userAggregateRequest): URL => {
+    let url = base_url +`/aggs/ticker/${obj.stocksTicker}/range/${obj.multiplier}/${obj.timespan}/${obj.from}/${obj.to}?`;
     url += obj.adjusted? `adjusted=${obj.adjusted}` : '';
     url += obj.sort? `&sort=${obj.sort}`: '';
     url += obj.limit? `&limit=${obj.limit}` : '';
     url += `&apiKey=${config.poly_key}`;
 
-    return url;
+    const apiRequestURL = new URL(url);
+    return apiRequestURL;
 }
+
 
 export {
     getAggregate,
